1e1737c6fc3dd6875f0d35ec1629d9e7ec0fbb43
GustavoHerreroNunes <ghnunes2010@gmail.com>
2020-03-27 11:12:33 -0300

    123...

---
+++ Teste.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Testando 1 2...</title>
+	</head>
+	
+	<body>
+		<?php
+			for($x =0; $x < 10; $x++){
+				echo "Hello World, testando, 1 2 3,<br>";
+			}
+		?>
+	</body>
+	
+</html>
